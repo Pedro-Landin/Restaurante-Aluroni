a431006730b2cdae56a8dcbd7a2eb7b316bb4f6a
Pedro landim <pedrolandin@pandanjo.com>
2023-08-18 10:55:21 -0300

    Lista de intes

---
+++ src/pages/Caradapio/Itens/index.tsx
@@ -0,0 +1,13 @@
+import Cardapio from "./itens.json"
+import Item from "./Item"
+import styles from "./Itens.module.scss";
+
+export default function Itens() {
+    return(
+        <div className={styles.itens}>
+            {Cardapio.map((item) => (
+                <Item key={item.id} {...item}/>
+            ))}
+        </div>
+    )
+}
