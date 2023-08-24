8e13b16c7c4422ef20296651b21f3cce98aac738
Pedro landim <henriquep35036@gmail.com>
2023-08-24 14:08:58 -0300

    Estilo Tema

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Inicio from "pages/inicio";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function AppRouter() {
+
   return (
     <main>
       <Router>
